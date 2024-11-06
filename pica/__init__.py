31a1940cc2702353855e6339d38cb0aa0b421760
Daniel Seipt <daniel@MacBookPro.fritz.box>
2024-11-07 00:04:42 +0100

    fixed yaml float loading

--- pica/__init__.py
+++ pica/__init__.py
@@ -6,4 +6,17 @@ import numpy as np
 from .constants import *
 from .collisions import *
 
-# __version__ = read('__version__.py')
+
+import re
+float_loader = yaml.SafeLoader
+float_loader.add_implicit_resolver(
+    u'tag:yaml.org,2002:float',
+    re.compile(u'''^(?:
+     [-+]?(?:[0-9][0-9_]*)\\.[0-9_]*(?:[eE][-+]?[0-9]+)?
+    |[-+]?(?:[0-9][0-9_]*)(?:[eE][-+]?[0-9]+)
+    |\\.[0-9_]+(?:[eE][-+][0-9]+)?
+    |[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\\.[0-9_]*
+    |[-+]?\\.(?:inf|Inf|INF)
+    |\\.(?:nan|NaN|NAN))$''', re.X),
+    list(u'-+0123456789.'))
+
